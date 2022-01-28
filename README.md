d50de2024833cd68445c5b02e7efd901f575be31
Andres <anferben@gmail.com>
2022-01-28 11:09:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# data-engineering
+
+By: Andres FB
+notebooks for data engineering zoomcamp 
+
+## License
---
+++ pipeline.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import sys
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some interesting things happen here
+
+print(f'Job completed successfully for day {day}')
